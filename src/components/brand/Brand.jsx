fca2395a42a9f825ac3a487cea144e9d2c88fe36
edmarsht <edtoulet@gmail.com>
2021-11-25 19:43:31 +0100

    brand correct

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -9,11 +9,14 @@ import shopify from "../../assets/shopify.png"
 function Brand() {
     return (
         <div className="hook__brand">
+        Over 80,000 paying customers trust SendGrid to send more than 90 billion emails every month.
+        <div className="hook__brand-images">
             <img src={google} alt="google" />
             <img src={slack} alt="slack" />
             <img src={atlassian} alt="atlassian" />
             <img src={dropbox} alt="dropbox" />
             <img src={shopify} alt="shopify" />
+        </div>
         </div>
     )
 }
