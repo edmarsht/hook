b35b61ad3f0297e79cb1ef26ff14a93905165d8a
edmarsht <edtoulet@gmail.com>
2021-11-25 19:31:38 +0100

    add responsive brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Brand.css";
+import google from "../../assets/google.png"
+import slack from "../../assets/slack.png"
+import atlassian from "../../assets/atlassian.png"
+import dropbox from "../../assets/dropbox.png"
+import shopify from "../../assets/shopify.png"
 
 function Brand() {
     return (
-        <div>
-            Brand
+        <div className="hook__brand">
+            <img src={google} alt="google" />
+            <img src={slack} alt="slack" />
+            <img src={atlassian} alt="atlassian" />
+            <img src={dropbox} alt="dropbox" />
+            <img src={shopify} alt="shopify" />
         </div>
     )
 }
