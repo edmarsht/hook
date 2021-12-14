b3912acaea7a33156a408c247c01a268da2ac6e7
edmarsht <edtoulet@gmail.com>
2021-12-14 16:30:20 +0100

    footer & responsive finished

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Footer.css";
+import logo from "../../assets/logo.svg";
 
 function Footer() {
   return (
@@ -10,6 +11,33 @@ function Footer() {
         </h1>
         <button className="hook__footer_button"> Hover me</button>
       </div>
+      <div className="hook__container-details">
+        <div className="hook__container-details-1">
+          <img src={logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="hook__container-details-2">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="hook__container-details-3">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="hook__container-details-4">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="div"></div>
+      <h5 className="hook__container-right">© 2021 HOOK. All rights reserved.</h5>
     </div>
   );
 }
